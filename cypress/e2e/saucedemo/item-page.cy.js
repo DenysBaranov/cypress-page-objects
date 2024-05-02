a233c7c83a88c967af897aba6decec6acc3e95e0
denys <work.deniell90@gmail.com>
2024-05-03 00:26:09 +0300

    Moving the method to the before hook

--- cypress/e2e/saucedemo/item-page.cy.js
+++ cypress/e2e/saucedemo/item-page.cy.js
@@ -5,10 +5,10 @@ import ItemPage from "../../page-objects/pages/ItemPage";
 describe("Tests item page elements", () => {
   beforeEach("Login on site", () => {
       ItemPage.open();
+      ItemPage.clickOnItemNameByIndex(1);
     }); 
 
   it("Check the picture display, description, price and product name", () => {
-    ItemPage.clickOnItemNameByIndex(1);
     ItemPage.itemImg.should("be.visible");
     ItemPage.itemDesc.should("be.visible");
     ItemPage.itemPrice.should("be.visible");
@@ -16,13 +16,11 @@ describe("Tests item page elements", () => {
   });
 
   it("Check the display of the Remove button after clicking on the Add to cart button", () => {
-    ItemPage.clickOnItemNameByIndex(1);
     ItemPage.clickAddToCartButton();
     ItemPage.removeButton.should("be.visible");
   });
 
   it("Check the display of the Add to cart button after clicking on the Remove button", () => {
-    ItemPage.clickOnItemNameByIndex(1);
     ItemPage.clickAddToCartButton();
     ItemPage.clickRemoveButton();
     ItemPage.addToCartButton.should("be.visible");
